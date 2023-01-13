d6023da1633d890c9ace5186bcdfd9fa02722356
Sergey Dolin <dsame@github.com>
2023-01-13 14:55:40 -0800

    first commit

---
+++ Repro65/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Repro65
+//
+//  Created by Dolin Sergey on 12.1.23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Repro65/Repro65App.swift
@@ -0,0 +1,17 @@
+//
+//  Repro65App.swift
+//  Repro65
+//
+//  Created by Dolin Sergey on 12.1.23..
+//
+
+import SwiftUI
+
+@main
+struct Repro65App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
